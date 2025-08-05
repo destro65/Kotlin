3f7f1ef81a44be2ea1f730dca613ae92430c0ca7
harol escritorio <harol.lady@hotmail.com>
2025-08-05 10:47:42 -0500

    general 05/08/25

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -52,7 +52,7 @@ class MainScreen : Screen {
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = {
                 navigator.push(SecondScreen())
-            }) { Text(text="Navegacion dasdad") }
+            }) { Text(text="Navegacion dasdada") }
         }
 
     }
