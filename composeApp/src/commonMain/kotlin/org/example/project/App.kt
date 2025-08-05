1c4c5e7545fdba116f98dc3047c0dfaddb4bf35c
harol escritorio <harol.lady@hotmail.com>
2025-08-05 10:37:07 -0500

    general 05/08

---
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,71 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import pruebakotlin.composeapp.generated.resources.Res
+import pruebakotlin.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Navigator(screen = MainScreen())
+    }
+}
+
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.Blue),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text= "Primera pantalla", fontSize = 26.sp, color = Color.White)
+    }
+}
+
+class MainScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator: Navigator = LocalNavigator.currentOrThrow
+
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = {
+                navigator.push(SecondScreen())
+            }) { Text(text="Navegacion") }
+        }
+
+    }
+}
+
+class SecondScreen : Screen {
+    @Composable
+    override fun Content() {
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text= "Segunda pantalla", fontSize = 26.sp, color = Color.White)
+        }
+    }
+}
