9c383e528bcce8feb519a77ddd2ae12f397f5d63
harol escritorio <harol.lady@hotmail.com>
2025-08-12 09:17:36 -0500

    boton de regreso añadido

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -18,13 +18,16 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Navigator(screen = MainScreen())
+        Navigator(screen = MainScreen()){navigator ->
+            SlideTransition(navigator)
+        }
     }
 }
 
